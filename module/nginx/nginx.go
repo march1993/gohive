fe3a54e949b2db761914d42df2ff984c558a46cd
Wenhua Shi <march511@gmail.com>
2018-01-29 23:44:42 +0100

    update

--- module/nginx/nginx.go
+++ module/nginx/nginx.go
@@ -2,27 +2,36 @@ package nginx
 
 import (
 	"encoding/json"
+	"github.com/march1993/gohive/api"
 	"github.com/march1993/gohive/config"
 	"github.com/march1993/gohive/module"
 	"github.com/march1993/gohive/module/linux"
 	"github.com/march1993/gohive/util"
+	"io/ioutil"
+	"os/exec"
 	"strconv"
+	"strings"
 )
 
+type nginx struct{}
+
 func init() {
-	// module.RegisterModule("profile", &profile{})
+	module.RegisterModule("nginx", &nginx{})
 	module.RegisterRuncom(runcomHandler)
 }
 
+const (
+	HOST = "127.0.0.1"
+)
+
 func runcomHandler(name string) []string {
 
-	host := "127.0.0.1"
 	port := strconv.FormatUint(uint64(getPort(name)), 10)
 
 	return []string{
-		"export HOST=" + host,
+		"export HOST=" + HOST,
 		"export PORT=" + port,
-		"export HOST_PORT=" + host + ":" + port,
+		"export HOST_PORT=" + HOST + ":" + port,
 	}
 }
 
@@ -47,4 +56,90 @@ func getPort(name string) uint16 {
 	}
 
 	panic("APP_N_LIMIT: No more available port to assign")
+}
+
+const (
+	NGINX_CONF_TEMPLATE = "./templates/app.nginx.conf"
+)
+
+func (n *nginx) Create(name string) api.Status {
+
+	bytes, err := ioutil.ReadFile(NGINX_CONF_TEMPLATE)
+	if err != nil {
+		return api.Status{
+			Status: api.STATUS_FAILURE,
+			Reason: err.Error(),
+		}
+	}
+
+	ServerName := name + "." + config.Get("server_name", "_")
+	ProxyPass := "http://" + HOST + ":" + strconv.FormatUint(uint64(getPort(name)), 10)
+
+	content := string(bytes)
+	content = strings.Replace(content, "{{ServerName}}", ServerName, -1)
+	content = strings.Replace(content, "{{ProxyPass}}", ProxyPass, -1)
+
+	config.AppConfigSet(name, "nginx", "hash", util.Hash(content))
+	output := config.GetDataDir(name) + "/nginx.conf"
+	err = ioutil.WriteFile(output, []byte(content), 0644)
+	if err != nil {
+		return api.Status{
+			Status: api.STATUS_FAILURE,
+			Reason: err.Error(),
+		}
+	}
+
+	return api.Status{Status: api.STATUS_SUCCESS}
+}
+
+func (n *nginx) Rename(oldName, newName string) api.Status {
+	return n.Repair(newName)
+}
+
+func (n *nginx) Remove(name string) api.Status {
+	return nginxReload()
+}
+
+func (n *nginx) Status(name string) api.Status {
+
+	hash := config.AppConfigGet(name, "nginx", "hash", "")
+	output := config.GetDataDir(name) + "/nginx.conf"
+
+	bytes, err := ioutil.ReadFile(output)
+	if err != nil {
+		return api.Status{
+			Status: api.STATUS_FAILURE,
+			Reason: err.Error(),
+		}
+	}
+
+	if hash != util.Hash(string(bytes)) || "" == hash {
+		return api.Status{
+			Status: api.STATUS_FAILURE,
+			Reason: api.PROFILE_BASHRC_EXPIRED,
+		}
+	}
+
+	return api.Status{Status: api.STATUS_SUCCESS}
+}
+
+func (n *nginx) Repair(name string) api.Status {
+	return n.Create(name)
+}
+
+func (n *nginx) ListRemoved() []string {
+	return []string{}
+}
+
+func nginxReload() api.Status {
+	output, err := exec.Command("systemctl", "reload", "nginx").CombinedOutput()
+	if err != nil {
+		return api.Status{
+			Status: api.STATUS_FAILURE,
+			Reason: string(output),
+		}
+	} else {
+
+		return api.Status{Status: api.STATUS_SUCCESS}
+	}
 }
