608bbd8fcaf5d5f83672cf8e8c994a3e33e235f7
Wenhua Shi <march511@gmail.com>
2018-01-29 02:29:12 +0100

    add git post-update hook

--- module/git/git.go
+++ module/git/git.go
@@ -5,10 +5,16 @@ import (
 	"github.com/march1993/gohive/config"
 	"github.com/march1993/gohive/module"
 	_ "github.com/march1993/gohive/module/linux"
+	"github.com/march1993/gohive/util"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
+)
+
+const (
+	gitPostUpdateTemplate  = "./templates/post-update"
+	gitPostUpdateGenerated = "/repo.git/hooks/post-update"
 )
 
 type git struct{}
@@ -19,19 +25,41 @@ func init() {
 
 func (g *git) Create(name string) api.Status {
 	unixname := config.APP_PREFIX + name
+	errs := []string{}
+
+	/* 1. create repo.git */
 	stdout, err := exec.Command("runuser",
 		unixname,
 		"-s", "/bin/bash",
 		"-c", "cd ~ && git init --bare repo.git",
 	).CombinedOutput()
 	if err != nil {
+		errs = append(errs, string(stdout))
+
+	}
+
+	/* 2. copy post-update hook file */
+	generated := config.GetHomeDir(name) + gitPostUpdateGenerated
+	bytes, err := ioutil.ReadFile(gitPostUpdateTemplate)
+	if err != nil {
+		errs = append(errs, err.Error())
+	} else {
+		err = ioutil.WriteFile(generated, bytes, 0755)
+
+		if err != nil {
+			errs = append(errs, err.Error())
+		}
+	}
+
+	if len(errs) > 0 {
 		return api.Status{
 			Status: api.STATUS_FAILURE,
-			Reason: string(stdout),
+			Reason: strings.Join(errs, "\n"),
 		}
 	} else {
 		return api.Status{Status: api.STATUS_SUCCESS}
 	}
+
 }
 
 func (g *git) Rename(oldName string, newName string) api.Status {
@@ -46,34 +74,26 @@ func (g *git) Remove(name string) api.Status {
 
 func (g *git) Status(name string) api.Status {
 	unixname := config.APP_PREFIX + name
+	errs := []string{}
+
 	stdout, err := exec.Command("runuser",
 		unixname,
 		"-s", "/bin/bash",
 		"-c", "cd ~/repo.git && git branch -a",
 	).CombinedOutput()
 	if err != nil {
-		return api.Status{
-			Status: api.STATUS_FAILURE,
-			Reason: string(stdout),
-		}
-	} else {
-		return api.Status{
-			Status: api.STATUS_SUCCESS,
-			Result: string(stdout),
-		}
-	}
-}
-
-func (g *git) Repair(name string) api.Status {
-	unixname := config.APP_PREFIX + name
-	errs := []string{}
-
-	if stdout, err := exec.Command("runuser",
-		unixname,
-		"-s", "/bin/bash",
-		"-c", "cd ~ && git init --bare repo.git",
-	).CombinedOutput(); err != nil {
 		errs = append(errs, string(stdout))
+	}
+
+	generated, _ := ioutil.ReadFile(config.GetHomeDir(name) + gitPostUpdateGenerated)
+	template, err := ioutil.ReadFile(gitPostUpdateTemplate)
+	if err != nil {
+		errs = append(errs, err.Error())
+	} else {
+
+		if util.Hash(string(template)) != util.Hash(string(generated)) {
+			errs = append(errs, "Git hook file is incorrect.")
+		}
 	}
 
 	if len(errs) > 0 {
@@ -84,7 +104,10 @@ func (g *git) Repair(name string) api.Status {
 	} else {
 		return api.Status{Status: api.STATUS_SUCCESS}
 	}
-	// TODO: Repair other problems
+}
+
+func (g *git) Repair(name string) api.Status {
+	return g.Create(name)
 }
 
 func (g *git) ListRemoved() []string {
